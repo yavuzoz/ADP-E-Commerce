7298156a5d3eab81451bfde463f78d790d659ba0
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-14 20:24:58 +0200

    JSX Lists

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 const products = [
-    { name: "product 1", price: 1000 },
-    { name: "product 2", price: 2000 },
-    { name: "product 3", price: 3000 },
+    { id: 1, name: "product 1", price: 1000, is_active: true },
+    { id: 2, name: "product 2", price: 2000, is_active: false },
+    { id: 3, name: "product 3", price: 3000, is_active: true },
 ]
 
 function App() {
@@ -25,19 +25,23 @@ function ProductList() {
     return (
         <div>
             <h2>ProductList</h2>
-            <Product product={products[0]} />
-            <Product product={products[1]} />
-            <Product product={products[2]} />
+            {products.map(p => (
+                <Product key={p.id} product={p} />
+            ))}
         </div>
     );
 }
 
 function Product(props: any) {
     return (
-        <div>
-            <h3>{props.product.name}</h3>
-            <p>{props.product.price}</p>
-        </div>
+        <>
+            {props.product.is_active ? (
+                <div>
+                    <h3>{props.product.name}</h3>
+                    <p>{props.product.price}</p>
+                </div>
+            ) : <p>Urun satista degil</p>}
+        </>
     );
 }
 
