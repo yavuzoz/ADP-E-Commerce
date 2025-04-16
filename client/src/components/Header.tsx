b6ae6a0f8bcc5e531f4e4f5901698469edde9515
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-16 15:12:40 +0200

    NavLinks

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,20 +1,57 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, Button, Stack, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router";
+import IconButton from "@mui/material/IconButton";
+
+const links = [
+    { title: "Home", to: "/" },
+    { title: "Catalog", to: "/catalog" },
+    { title: "About", to: "/about" },
+    { title: "Contact", to: "/contact" },
+]
+
+const navStyles = {
+    color: "inherit",
+    textDecoration: "none",
+    "&:hover": {
+        color: "text.primary",
+    },
+    "&.active": { color: "warning.main" }
+}
 
 export default function Header(props: any) {
     return (
         <AppBar position="static" sx={{ mb: 4 }}>
 
-            <Toolbar>
+            <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
 
-                <Container>
-                    <Typography variant="h6">
-                        E-commerce
-                    </Typography>
-                </Container>
+                <Box sx={{ display: "flex", alingItems: "center" }}>
+
+                    <Typography variant="h6">E-commerce </Typography>
+                    <Stack direction="row">
+                        {
+                            links.map(link =>
+                                <Button component={NavLink} to={link.to} sx={navStyles}>{link.title}</Button>
+                            )
+                        }
+                    </Stack>
 
 
-            </Toolbar>
+                </Box>
 
-        </AppBar>
+                <Box>
+
+                    <IconButton size="large" edge="start" color="inherit">
+                        <Badge badgeContent="2" color="secondary">
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+
+
+                </Box>
+
+            </Toolbar >
+
+        </AppBar >
     );
 }
