97757b2c724ddda308ed8d3a8b21907bc00e1a1c
Yavuz Özbay <ozbayavz@gmail.com>
2025-04-17 08:25:53 +0200

    Add ErrorController with standard HTTP error responses

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet("not-found")]
+        public IActionResult NotFoundError()
+        {
+            return NotFound();  // 404
+        }
+
+        [HttpGet("bad-request")]
+        public IActionResult BadRequestError()
+        {
+            return BadRequest();  // 400
+        }
+
+        [HttpGet("unauthorized")]
+        public IActionResult UnAuthorizedError()
+        {
+            return Unauthorized();  // 401
+        }
+
+        [HttpGet("validation-error")]
+        public IActionResult ValidationError()
+        {
+            ModelState.AddModelError("validation error 1", "vadalition error details");
+            ModelState.AddModelError("validation error 2", "vadalition error details");
+            return ValidationProblem();
+        }
+
+        [HttpGet("server-error")]
+        public IActionResult ServerError()
+        {
+            return Problem("An unexpected error occurred. Please try again later.");
+        }
+
+    }
+}
