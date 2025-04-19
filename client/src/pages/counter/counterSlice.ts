6864fe314e7935ccb5c3966efc3a019bb5f5fc2f
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-20 00:45:54 +0200

    Add typed useDispatch and useSelector hooks for Redux

--- client/src/pages/counter/counterSlice.ts
+++ client/src/pages/counter/counterSlice.ts
@@ -1,4 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store/store";
+
 export interface CounterState {
     value: number;
 
@@ -26,4 +29,7 @@ export const counterSlice = createSlice({
     }
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;    
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
