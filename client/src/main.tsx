c23c29ad8843a60180625caa741a403e23848c85
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-19 17:52:09 +0200

    Wrap application with CartContextProvider in main entry file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router';
 import { router } from './router/Routes';
+import { CartContextProvider } from './context/CartContext';
 
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <CartContextProvider>
+
+            <RouterProvider router={router} />
+
+        </CartContextProvider>
+
+
     </StrictMode>,
 )
