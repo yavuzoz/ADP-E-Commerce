af9e49f8268d34c450ad89bc725d58ca8bc6a7c0
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-19 22:42:05 +0200

    Fix missing return in DeleteItemFromCart success response

--- API/Controllers/CartController.cs
+++ API/Controllers/CartController.cs
@@ -51,7 +51,8 @@ public class CartController : ControllerBase
 
         var result = await _context.SaveChangesAsync() > 0;
 
-        if (result) CreatedAtAction(nameof(GetCart), CartToDTO(cart));
+        if (result)
+            return CreatedAtAction(nameof(GetCart), CartToDTO(cart));
 
         return BadRequest(new ProblemDetails { Title = "Problem removing item from the cart" });
     }
