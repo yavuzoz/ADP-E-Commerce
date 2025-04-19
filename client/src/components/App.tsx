4847de5f5fff1aad9bd4a858b8d3d6f97ce4200a
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-19 18:17:56 +0200

    Fetch cart data on App mount and set it in CartContext

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import Header from "./Header";
-import { Container, CssBaseline } from "@mui/material";
+import { Container, CssBaseline, CircularProgress } from "@mui/material";
 import { Outlet } from "react-router";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import requests from "../../api/requests";
+import { useCartContext } from "../context/CartContext";
 
 function App() {
 
-   return (
+    const { setCart } = useCartContext();
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        requests.Cart.get()
+            .then(cart => setCart(cart))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }, []);
+
+    if (loading) return <CircularProgress />;
+
+
+    return (
         <>
             <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
             <CssBaseline />
@@ -17,6 +34,5 @@ function App() {
         </>
     );
 }
-
 
 export default App
