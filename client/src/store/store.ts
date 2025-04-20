871e641aff7012dd6ef1da0479f2db7f2640141c
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-20 13:10:25 +0200

    Added  for catalogSlice

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { counterSlice } from '../pages/counter/counterSlice';
 import { cartSlice } from '../pages/catalog/cart/cartSlice';
+import { catalogSlice } from '../pages/catalog/catalogSlice'; // Added import for catalogSlice
 
 export const store = configureStore({
 
     reducer: {
         counter: counterSlice.reducer,
         cart: cartSlice.reducer,
+        catalog: catalogSlice.reducer,
     }
 
 })
