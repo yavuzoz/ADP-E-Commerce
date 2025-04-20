a45cbf8789722a0b76a28a72138385c9158b8d75
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-20 13:25:44 +0200

    fix(catalog): call fetchProducts thunk function in useEffect

--- client/src/pages/catalog/CatalogPage.tsx
+++ client/src/pages/catalog/CatalogPage.tsx
@@ -1,19 +1,21 @@
 import { useEffect } from "react";
 import ProductList from "./ProductList";
 import { CircularProgress } from "@mui/material";
-import { useAppDispatch } from "../../hooks/hooks";
-import { fetchProducts } from "./catalogSlice";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { fetchProducts, selectAllProducts } from "./catalogSlice";
 
 export default function CatalogPage() {
 
-    const products = useAppSelector(state => state.catalog.products);
+    const products = useAppSelector(selectAllProducts);
+    const { status } = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
-        dispatch(fetchProducts);
+        dispatch(fetchProducts())
+            ;
     }, []);
 
-    if (loading) return <CircularProgress />;
+    if (status === "pendingFetchProducts") return <CircularProgress />;
 
     return (
 
