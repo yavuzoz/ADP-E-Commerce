78c7831664c8ac1589e56b78955cf0cadc60d60a
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-20 11:33:13 +0200

    Register cartSlice reducer in Redux store

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { counterSlice } from '../pages/counter/counterSlice';
+import { cartSlice } from '../pages/catalog/cart/cartSlice';
 
 export const store = configureStore({
 
     reducer: {
-        counter: counterSlice.reducer
+        counter: counterSlice.reducer,
+        cart: cartSlice.reducer,
     }
 
 })
