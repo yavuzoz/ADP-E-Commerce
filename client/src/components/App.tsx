2489fe06178e4d5680896c77bdd94075bda04833
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-20 11:38:43 +0200

    Load cart data on app mount and dispatch to Redux store

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -5,23 +5,22 @@ import { Outlet } from "react-router";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import requests from "../../api/requests";
-import { useCartContext } from "../context/CartContext";
+import { useAppDispatch } from "../hooks/hooks";
+import { setCart } from "../pages/catalog/cart/cartSlice"; // Added import
 
 function App() {
 
-    const { setCart } = useCartContext();
+    const distpatch = useAppDispatch();
     const [loading, setLoading] = useState(true);
-
 
     useEffect(() => {
         requests.Cart.get()
-            .then(cart => setCart(cart))
+            .then(cart => distpatch(setCart(cart)))
             .catch(error => console.log(error))
             .finally(() => setLoading(false))
     }, []);
 
     if (loading) return <CircularProgress />;
-
 
     return (
         <>
