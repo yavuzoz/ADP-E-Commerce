2432a17abdc16e064b68dccbe37052d80f0bd39d
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-22 15:42:47 +0200

    feat: implement async thunks and reducers for cart operations (add, delete, get)

--- client/src/pages/catalog/cart/cartSlice.ts
+++ client/src/pages/catalog/cart/cartSlice.ts
@@ -9,40 +9,47 @@ interface CartState {
 
 const initialState: CartState = {
     cart: null,
-    status: 'idle'
+    status: "idle"
 }
 
 export const addItemToCart = createAsyncThunk<Cart, { productId: number, quantity?: number }>(
     "cart/addItemToCart",
     async ({ productId, quantity = 1 }) => {
         try {
-            return await requests.Cart.addItem(productId, quantity)
+            return await requests.Cart.addItem(productId, quantity);
         }
         catch (error) {
-            console.log(error)
+            console.log(error);
         }
-
     }
-)
+);
 
 export const deleteItemFromCart = createAsyncThunk<Cart, { productId: number, quantity?: number, key?: string }>(
     "cart/deleteItemFromCart",
     async ({ productId, quantity = 1 }) => {
         try {
-            return await requests.Cart.deleteItem(productId, quantity)
+            return await requests.Cart.deleteItem(productId, quantity);
         }
         catch (error) {
-            console.log(error)
+            console.log(error);
         }
+    }
+);
 
-
-
+export const getCart = createAsyncThunk<Cart>(
+    "cart/getcart",
+    async (_, thunkAPI) => {
+        try {
+            return await requests.Cart.get();
+        }
+        catch (error: any) {
+            return thunkAPI.rejectWithValue({ error: error.data });
+        }
     }
 )
 
-
 export const cartSlice = createSlice({
-    name: 'cart',
+    name: "cart",
     initialState,
     reducers: {
         setCart: (state, action) => {
@@ -51,32 +58,40 @@ export const cartSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder.addCase(addItemToCart.pending, (state, action) => {
-            console.log(action)
-            state.status = 'pendingAddItem' + action.meta.arg.productId;
+            console.log(action);
+            state.status = "pendingAddItem" + action.meta.arg.productId;
         });
 
         builder.addCase(addItemToCart.fulfilled, (state, action) => {
-            console.log(action)
+            console.log(action);
             state.cart = action.payload;
-            state.status = 'idle'
+            state.status = "idle";
         });
 
         builder.addCase(addItemToCart.rejected, (state) => {
-            state.status = 'idle'
+            state.status = "idle";
         });
 
         builder.addCase(deleteItemFromCart.pending, (state, action) => {
-            console.log(action)
-            state.status = 'pendingDeleteItem' + action.meta.arg.productId + action.meta.arg.key;
+            console.log(action);
+            state.status = "pendingDeleteItem" + action.meta.arg.productId + action.meta.arg.key;
         });
 
         builder.addCase(deleteItemFromCart.fulfilled, (state, action) => {
             state.cart = action.payload;
-            state.status = 'idle'
+            state.status = "idle";
         });
 
         builder.addCase(deleteItemFromCart.rejected, (state) => {
-            state.status = 'idle'
+            state.status = "idle";
+        });
+
+        builder.addCase(getCart.fulfilled, (state, action) => {
+            state.cart = action.payload;
+        });
+
+        builder.addCase(getCart.rejected, (_, action) => {
+            console.log(action.payload);
         });
     }
 })
