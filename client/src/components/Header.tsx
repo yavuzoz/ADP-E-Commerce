eece470be7312433a82e6698bea7027325f3979e
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-22 14:47:38 +0200

    feat: show user info and logout button in header based on authentication state

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,8 @@ import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, Badge, Box, Button, Stack, Toolbar, Typography } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import { NavLink, Link } from "react-router-dom"; // Added import from "react-router-dom"
-import { useAppSelector } from "../hooks/hooks"; // Added import from "../hooks/hooks"
+import { useAppSelector, useAppDispatch } from "../hooks/hooks"; // Added import from "../hooks/hooks"
+import { logout } from "../pages/account/accountSlice"; // Added import for logout action
 
 const links = [
     { title: "Home", to: "/" },
@@ -12,66 +13,70 @@ const links = [
     { title: "Error", to: "/error" },
 ]
 
-const autLinks = [
+const authLinks = [
     { title: "Login", to: "/login" },
-    { title: "Register", to: "/register" },
+    { title: "Register", to: "/register" }
 ]
 
 const navStyles = {
     color: "inherit",
     textDecoration: "none",
     "&:hover": {
-        color: "text.primary",
+        color: "text.primary"
     },
-    "&.active": { color: "warning.main" }
+    "&.active": {
+        color: "warning.main"
+    }
 }
 
 export default function Header() {
+    const { cart } = useAppSelector(state => state.cart);
+    const { user } = useAppSelector(state => state.account);
+    const dispatch = useAppDispatch();
 
-    const { cart } = useAppSelector((state => state.cart)); // Assuming you have a selector to get the cart state
-    const itemcount = cart ? cart.cartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+    const itemCount = cart?.cartItems.reduce((total, item) => total + item.quantity, 0);
 
     return (
         <AppBar position="static" sx={{ mb: 4 }}>
-
             <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-
                 <Box sx={{ display: "flex", alignItems: "center" }}>
-
-                    <Typography variant="h6">E-commerce </Typography>
-
+                    <Typography variant="h6">E-Commerce</Typography>
 
                     <Stack direction="row">
-                        {
-                            links.map(link =>
-                                <Button key={link.to} component={NavLink} to={link.to} sx={navStyles}>{link.title}</Button>
-                            )
-                        }
+                        {links.map(link =>
+                            <Button key={link.to} component={NavLink} to={link.to} sx={navStyles}>{link.title}</Button>
+                        )}
                     </Stack>
-
 
                 </Box>
 
-                <Box>
-
+                <Box sx={{ display: "flex", alignItems: "center" }}>
                     <IconButton component={Link} to="/cart" size="large" edge="start" color="inherit">
-                        <Badge badgeContent={itemcount} color="secondary">
+                        <Badge badgeContent={itemCount} color="secondary">
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
 
-                    <Stack direction="row">
-                        {
-                            autLinks.map(link =>
-                                <Button key={link.to} component={NavLink} to={link.to} sx={navStyles}>{link.title}</Button>
-                            )
-                        }
-                    </Stack>
+                    {
+                        user ? (
+                            <Stack direction="row">
+                                <Button sx={navStyles}>{user.name}</Button>
+                                <Button sx={navStyles} onClick={() => dispatch(logout())}>Log Out</Button>
+                            </Stack>
+                        ) : (
+                            <Stack direction="row">
+                                {
+                                    authLinks.map(link =>
+                                        <Button key={link.to} component={NavLink} to={link.to} sx={navStyles}>{link.title}</Button>)
+                                }
+                            </Stack>
+                        )
+                    }
+
 
                 </Box>
 
-            </Toolbar >
-
-        </AppBar >
+            </Toolbar>
+        </AppBar>
     );
 }
