9479940bb221f68da5cf4442358a72ed4a237f83
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-22 13:42:36 +0200

    feat: build styled Login page with MUI components and form layout

--- client/src/pages/account/loginPage.tsx
+++ client/src/pages/account/loginPage.tsx
@@ -1,6 +1,23 @@
+import { LockOutlined } from "@mui/icons-material";
+import { Container, Paper, Avatar, Typography, Box, TextField, Button } from "@mui/material";
+
 export default function LoginPage() {
 
+    function handleSubmit() { }
+
     return (
-        <h1>Login Page</h1>
+        <Container maxWidth="xs">
+            <Paper sx={{ marginTop: 8, padding: 2 }} elevation={3}>
+                <Avatar sx={{ mx: "auto", color: "secondary.main", textAlign: "center", mb: 1 }}>
+                    <LockOutlined />
+                </Avatar>
+                <Typography component="h1" variant="h5" sx={{ textAlign: "center" }}>Login</Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 2 }}>
+                    <TextField label="Enter username" fullWidth required autoFocus sx={{ mb: 2 }} size="small"></TextField>
+                    <TextField label="Enter password" type="password" fullWidth required sx={{ mb: 2 }} size="small"></TextField>
+                    <Button type="submit" variant="contained" fullWidth sx={{ mt: 1 }}>Login</Button>
+                </Box>
+            </Paper>
+        </Container>
     );
 }
