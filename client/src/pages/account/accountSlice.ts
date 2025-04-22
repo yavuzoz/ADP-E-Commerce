187aeb3f0ac65b980829c5e3260508654f9b0b84
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-22 15:44:43 +0200

    feat: add getUser thunk with localStorage restore and rejection handling

--- client/src/pages/account/accountSlice.ts
+++ client/src/pages/account/accountSlice.ts
@@ -4,7 +4,6 @@ import { FieldValues } from "react-hook-form";
 import requests from "../../../api/requests";
 import { router } from "@/router/Routes";
 
-
 interface AccountState {
     user: User | null;
 }
@@ -27,6 +26,26 @@ export const loginUser = createAsyncThunk<User, FieldValues>(
     }
 )
 
+export const getUser = createAsyncThunk<User>(
+    "account/getuser",
+    async (_, thunkAPI) => {
+        thunkAPI.dispatch(setUser(JSON.parse(localStorage.getItem("user")!)));
+        try {
+            const user = await requests.Account.getUser();
+            localStorage.setItem("user", JSON.stringify(user));
+            return user;
+        }
+        catch (error: any) {
+            return thunkAPI.rejectWithValue({ error: error.data });
+        }
+    },
+    {
+        condition: () => {
+            if (!localStorage.getItem("user")) return false;
+        }
+    }
+);
+
 export const accountSlice = createSlice({
     name: "account",
     initialState,
@@ -40,12 +59,21 @@ export const accountSlice = createSlice({
             state.user = action.payload;
         }
     },
-    extraReducers: (builder) => {
+    extraReducers: (builder => {
         builder.addCase(loginUser.fulfilled, (state, action) => {
             state.user = action.payload;
-        });
-    }
-});
+        })
 
+        builder.addCase(getUser.fulfilled, (state, action) => {
+            state.user = action.payload;
+        })
+
+        builder.addCase(getUser.rejected, (state) => {
+            state.user = null;
+            localStorage.removeItem("user");
+            router.navigate("/login");
+        })
+    })
+})
 
 export const { logout, setUser } = accountSlice.actions;
