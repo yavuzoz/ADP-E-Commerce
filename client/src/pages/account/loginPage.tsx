8b8755612930efc1ea97cadcbc9e5c29d04d1574
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-22 14:11:40 +0200

    feat: integrate react-hook-form into Login page with API submission

--- client/src/pages/account/loginPage.tsx
+++ client/src/pages/account/loginPage.tsx
@@ -1,24 +1,18 @@
-import React, { useState } from "react";
 import { LockOutlined } from "@mui/icons-material";
-import { Container, Paper, Avatar, Typography, Box, TextField, Button } from "@mui/material";
+import { Avatar, Box, Button, Container, Paper, TextField, Typography } from "@mui/material";
+import { FieldValues, useForm } from "react-hook-form";
 import requests from "../../../api/requests";
 
 export default function LoginPage() {
-
-    const [values, setValues] = useState({
-        username: "",
-        password: ""
+    const { register, handleSubmit } = useForm({
+        defaultValues: {
+            username: "",
+            password: ""
+        }
     });
 
-    function handleSubmit(e: any) {
-        e.preventDefault();
-        console.log(values);
-        requests.Account.login(values);
-    }
-
-    function handleInputChange(e: any) {
-        const { name, value } = e.target;
-        setValues({ ...values, [name]: value }); // { username: "abc", password: "123" }
+    async function submitForm(data: FieldValues) {
+        await requests.Account.login(data);
     }
 
     return (
@@ -28,20 +22,16 @@ export default function LoginPage() {
                     <LockOutlined />
                 </Avatar>
                 <Typography component="h1" variant="h5" sx={{ textAlign: "center" }}>Login</Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 2 }}>
+                <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 2 }}>
                     <TextField
-                        name="username"
-                        value={values.username}
-                        onChange={handleInputChange}
+                        {...register("username")}
                         label="Enter username"
                         fullWidth required autoFocus
                         sx={{ mb: 2 }}
                         size="small"></TextField>
 
                     <TextField
-                        name="password"
-                        value={values.password}
-                        onChange={handleInputChange}
+                        {...register("password")}
                         label="Enter password"
                         type="password"
                         fullWidth required
