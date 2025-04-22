94a29c9207d27c10f00a0817df3fef80b5ccee8c
Yasin Kereci <yke144907@stud.gibb.ch>
2025-04-22 13:44:44 +0200

    feat: implement controlled form and API login request on Login page

--- client/src/pages/account/loginPage.tsx
+++ client/src/pages/account/loginPage.tsx
@@ -1,9 +1,25 @@
+import React, { useState } from "react";
 import { LockOutlined } from "@mui/icons-material";
 import { Container, Paper, Avatar, Typography, Box, TextField, Button } from "@mui/material";
+import requests from "../../../api/requests";
 
 export default function LoginPage() {
 
-    function handleSubmit() { }
+    const [values, setValues] = useState({
+        username: "",
+        password: ""
+    });
+
+    function handleSubmit(e: any) {
+        e.preventDefault();
+        console.log(values);
+        requests.Account.login(values);
+    }
+
+    function handleInputChange(e: any) {
+        const { name, value } = e.target;
+        setValues({ ...values, [name]: value }); // { username: "abc", password: "123" }
+    }
 
     return (
         <Container maxWidth="xs">
@@ -13,8 +29,25 @@ export default function LoginPage() {
                 </Avatar>
                 <Typography component="h1" variant="h5" sx={{ textAlign: "center" }}>Login</Typography>
                 <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 2 }}>
-                    <TextField label="Enter username" fullWidth required autoFocus sx={{ mb: 2 }} size="small"></TextField>
-                    <TextField label="Enter password" type="password" fullWidth required sx={{ mb: 2 }} size="small"></TextField>
+                    <TextField
+                        name="username"
+                        value={values.username}
+                        onChange={handleInputChange}
+                        label="Enter username"
+                        fullWidth required autoFocus
+                        sx={{ mb: 2 }}
+                        size="small"></TextField>
+
+                    <TextField
+                        name="password"
+                        value={values.password}
+                        onChange={handleInputChange}
+                        label="Enter password"
+                        type="password"
+                        fullWidth required
+                        sx={{ mb: 2 }}
+                        size="small"></TextField>
+
                     <Button type="submit" variant="contained" fullWidth sx={{ mt: 1 }}>Login</Button>
                 </Box>
             </Paper>
